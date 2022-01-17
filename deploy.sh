014d3f5a33b8f8eb5d8e3151dd5522e0ba821f20
LordFarquhar <65400619+LordFarquhar@users.noreply.github.com>
2022-01-17 17:46:51 +0000

    add script

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,25 @@
 root=/
 
-current_branch = $(git branch --show-current)
+if [ -z "$1" ]
+then
+    echo "Please provide a commit message"
+else
+    echo "getting current branch name" &&
+	current_branch=$(git branch --show-current) &&
+	echo "name got \"$current_branch\""
+	echo "moving to gh-pages branch" &&
+	git checkout gh-pages &&
+	echi "getting dist folder" &&
+	git checkout master -- dist &&
+	echo "moving all files from dist into root dir" &&
+	mv dist/* dist/.* . && rmdir dist &&
+	echo "adding files to be tracked" &&
+	git add . && # add all files (that arent in gitignore) to the repo
+	echo "committing changes as \"$1\"" &&
+	git commit -m "$1" &&
+	echo "pushing to origin" &&
+	git push origin  &&
+	echo "returning to branch \"$current_branch\"" &&
+	git checkout main
+fi
 
-echo "moving to gh-pages branch"
-git checkout gh-pages
-echi "getting dist folder"
-git checkout master -- dist
-echo "moving all files from dist into root dir"
-mv dist/* dist/.* . && rmdir dist
-echo "adding files to be tracked"
-git add . # add all files (that arent in gitignore) to the repo
-echo "committing changes as \"$1\""
-git commit -m "$1"
-echo "pushing to origin"
-git push origin 
-echo "returning to branch \"{$current_branch}\""
-git checkout main
